e77fe8c102284a74e9367b717257f7cff73819cc
Jaben Cargman <jaben-accounts@tinygecko.com>
2019-01-12 12:24:34 -0500

    marked Authenticate protected.

--- src/Docker.Registry.DotNet/Authentication/AuthenticationProvider.cs
+++ src/Docker.Registry.DotNet/Authentication/AuthenticationProvider.cs
@@ -13,7 +13,7 @@ namespace Docker.Registry.DotNet.Authentication
         /// </summary>
         /// <param name="request"></param>
         /// <returns></returns>
-        internal abstract Task AuthenticateAsync(HttpRequestMessage request);
+        protected abstract Task AuthenticateAsync(HttpRequestMessage request);
 
         /// <summary>
         /// Called when the send is challenged.
@@ -21,6 +21,6 @@ namespace Docker.Registry.DotNet.Authentication
         /// <param name="request"></param>
         /// <param name="response"></param>
         /// <returns></returns>
-        internal abstract Task AuthenticateAsync(HttpRequestMessage request, HttpResponseMessage response);
+        protected abstract Task AuthenticateAsync(HttpRequestMessage request, HttpResponseMessage response);
     }
 }
